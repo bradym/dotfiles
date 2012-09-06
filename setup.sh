429dbd95e751402d98c0703f1e7a71560be63a01
Brady Mitchell <bradymitch@gmail.com>
2012-09-06 01:05:10 -0700

    Check for and backup existing dotfiles before creating symlinks.

--- setup.sh
+++ setup.sh
@@ -7,18 +7,46 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Where should symlinks be created?
-TARGET="$HOME/testing"
+if [[ -z "$1" ]]; then
+    TARGET="$HOME"
+else
+    TARGET=$1
+fi
+
+# If $TARGET does not exist, create it.
+if [[ ! -d $TARGET ]]; then
+    mkdir -v -p $TARGET
+fi
+
+# Function to check for existing file and rename
+function rename_existing (){
+    FILENAME=$1
+
+    if [[ -e "$TARGET/$FILENAME" ]] && [[ ! -L "$TARGET/$FILENAME" ]] || [[ -d "$TARGET/$FILENAME" ]]; then
+        OLDNAME=$FILENAME"_backup.$(date +%s)"
+        mv "$TARGET/$FILENAME" "$TARGET/$OLDNAME"
+        echo "Existing $FILENAME found and renamed to $OLDNAME"
+    fi
+}
+
 
 # --------------------------------------------------------
 # Setup bash
 # --------------------------------------------------------
-ln -v -s $DIR/.bashrc $TARGET/.bashrc
+
+# Check for existing .bashrc and rename
+rename_existing .bashrc
+
+# If a symlink exists for .bashrc, it will be modified 
+ln -nvfs $DIR/.bashrc $TARGET/.bashrc
 
 # Link correct prompt setup file
+rename_existing .bash_ps1
+
 if [[ $OSTYPE == *darwin* ]]; then
-    ln -v -s $DIR/.bash_ps1.osx $TARGET/.bash_ps1
+    ln -nvfs $DIR/.bash_ps1.osx $TARGET/.bash_ps1
 else
-    ln -v -s $DIR/.bash_ps1 $TARGET/.bash_ps1
+    ln -nvfs $DIR/.bash_ps1 $TARGET/.bash_ps1
 fi
 
 
@@ -26,8 +54,18 @@ fi
 # Setup vim
 # --------------------------------------------------------
 
-ln -v -s $DIR/.vimrc $TARGET/.vimrc
-ln -v -s $DIR/.vim $TARGET/.vim
-git --git-dir=/$DIR/.git --work-tree=$DIR submodule update --init
+# Check for existing .vimrc and rename
+rename_existing .vimrc
 
+# If a symlink exists for .vimrc it will be modified
+ln -nvfs $DIR/.vimrc $TARGET/.vimrc
 
+# If .vim folder exists, rename and notify user
+rename_existing .vim
+
+# Setup vim plugins
+git --git-dir=/$DIR/.git --work-tree=$DIR submodule init
+git --git-dir=/$DIR/.git --work-tree=$DIR submodule update 
+
+# If a symlink exists for .vim it will be modified
+ln -nvfs $DIR/.vim $TARGET/.vim
